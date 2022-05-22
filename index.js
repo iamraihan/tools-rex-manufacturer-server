ae0ffbec81b1ce0348d6d57ed4d4f6737db494f0
iamraihan <trueraihan@gmail.com>
2022-05-22 12:01:54 +0600

    products api added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
@@ -12,8 +13,31 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.2ngeg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("product-collection").collection("products");
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray()
+            console.log(result);
+            res.send(result)
+        })
+
+    } finally { }
+}
+run().catch(console.dir);
 
 
 
