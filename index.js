2eadf38d9d8a0e6157374c9cb955ce82f717abce
iamraihan <trueraihan@gmail.com>
2022-05-25 12:37:50 +0600

    jwt function added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -16,12 +17,28 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.2ngeg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
+
 
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db("product-collection").collection("products");
         const orderCollection = client.db("order-collection").collection("orders");
+        const userCollection = client.db("order-collection").collection("users");
 
         app.get('/products', async (req, res) => {
             const query = {}
@@ -38,35 +55,53 @@ async function run() {
         })
         app.put('/products/:id', async (req, res) => {
             const id = req.params.id
-            const update = req.body
+            const product = req.body
             console.log(update);
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updateDoc = {
-                $set: update
+                $set: product
             };
             const result = await productCollection.updateOne(filter, updateDoc, options);
             console.log(result);
             res.send(result)
         })
-        // app.put('/order/:email', async (req, res) => {
-        //     const email = req.params.email
-        //     const update = req.body
-        //     console.log(update);
-        //     const filter = { email: email };
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: update
-        //     };
-        //     const result = await productCollection.updateOne(filter, updateDoc, options);
-        //     console.log(result);
-        //     res.send(result)
-        // })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET);
+            res.send({ result, token })
+
+        })
+
         app.post('/order', async (req, res) => {
             const order = req.body
             const query = order
             const result = await orderCollection.insertOne(query)
             return res.send({ success: true, result })
+        })
+
+        app.get('/order/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const decodedEmail = req.decoded.email
+            const filter = { email: email }
+            const result = await orderCollection.find(filter).toArray()
+            res.send(result)
+
+        })
+
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)
         })
 
 
