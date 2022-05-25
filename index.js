6e769369811a99e57e31b1376cd03c4d8db7d486
iamraihan <trueraihan@gmail.com>
2022-05-25 21:53:31 +0600

    product delete api created

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ async function run() {
             const result = await cursor.limit(number).toArray()
             res.send(result)
         })
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        })
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id
             const filter = { _id: ObjectId(id) }
